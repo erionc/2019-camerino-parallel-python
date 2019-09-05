2004b81dd4bb5e5a864b7911b407b4beb54972fe
ASPP Student <student@localhost>
2019-09-05 10:00:29 +0200

    exe 1 solution takes 1 sec

--- 1_problem.py
+++ 1_problem.py
@@ -1,10 +1,20 @@
 from dask import delayed  # DON'T CHANGE (explained later)
+import time
+import math
 
 def func(i):
     """A dummy CPU-bound function."""
     print(f'Function {i} starting...')
     # ***INSERT CODE HERE*** (should take about 1 second to execute)
+    for i in range (800000000):
+        i += math.sqrt(2) 
     print(f'Function {i} done')
     return i
 
+
+start = time.time()
+
 lazy = [delayed(func)(i) for i in range(4)]  # DON'T CHANGE (explained later)
+
+end = time.time()
+print("it took {} seconds".format(end - start))
